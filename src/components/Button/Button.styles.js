aa0c23f30bde95a1c5ecd7154fbe201933370f69
ervinask <klimavicius.ervinas@gmail.com>
2022-06-08 16:48:18 +0300

    added styled-components

---
+++ src/components/Button/Button.styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  background: transparent;
+  border: 0.05rem solid black;
+  border-radius: 0.5rem;
+  padding: 0.5rem 1rem;
+`;
